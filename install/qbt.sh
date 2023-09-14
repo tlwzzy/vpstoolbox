c9bfec545a475c3335df3e726826f3061bc03eb3
dlutzy <36352458+tlwzzy@users.noreply.github.com>
2023-09-14 15:05:10 +0800

    Update qbt.sh

--- install/qbt.sh
+++ install/qbt.sh
@@ -45,7 +45,7 @@ mkdir /usr/share/nginx/qBittorrent/downloads/
 mkdir /usr/share/nginx/qBittorrent/data/
 mkdir /usr/share/nginx/qBittorrent/data/GeoIP/
 cd /usr/share/nginx/qBittorrent/data/GeoIP/
-curl -LO --progress-bar https://raw.githubusercontent.com/johnrosen1/vpstoolbox/master/binary/GeoLite2-Country.mmdb
+curl -LO --progress-bar https://raw.githubusercontent.com/tlwzzy/vpstoolbox/master/binary/GeoLite2-Country.mmdb
 cd
 chmod 755 /usr/share/nginx/
 chown -R nginx:nginx /usr/share/nginx/
